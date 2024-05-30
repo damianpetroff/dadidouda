76b2473d4b1c18a6a0e41efe600480c5c5bdc861
Damian Petroff <damian.petroff@outlook.com>
2024-05-31 00:00:42 +0200

    feat: all images + print

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,170 @@
+'use client';
+import { useState } from "react";
 import Image from "next/image";
 
+type PNGSelection = 1 | 2 | 3;
+
 export default function Home() {
+  const [pngTop, setPngTop] = useState<PNGSelection>(1)
+  const [pngMiddle, setPngMiddle] = useState<PNGSelection>(1)
+  const [pngBottom, setPngBottom] = useState<PNGSelection>(1)
+  const [name1, setName1] = useState<string>()
+  const [num, setNum] = useState<string>()
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="min-h-screen p-24 bg-gradient-to-br from-blue-300 to-cyan-200">
+      <div className="flex flex-col items-center justify-start gap-8 print:hidden">
+        <h1 className="font-extrabold tracking-tight text-5xl pb-10 bg-clip-text text-transparent bg-gradient-to-r from-blue-50 to-cyan-50">Crée ta carte Dadidouda</h1>
+        <div className="flex gap-8">
+          <div>
+            <label htmlFor="name" className="block text-sm font-medium leading-6">
+              Ton prénom
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="John"
+                onChange={(e) => setName1(e.target.value)}
+              />
+            </div>
+          </div>
+          <div>
+            <label htmlFor="name" className="block text-sm font-medium leading-6">
+              Ton numéro de membre
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="num"
+                id="num"
+                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="19123912"
+                onChange={(e) => setNum(e.target.value)}
+              />
+            </div>
+          </div>
         </div>
+        <div className="flex h-[400px] w-full gap-8 justify-center">
+          <div className="flex flex-col text-black bg-white aspect-[3/4] rounded-3xl shadow-xl">
+            <div className="flex h-full justify-center">
+              <div className="relative aspect-square">
+                <Image className="" src={`/img/avatar/top_${pngTop}.png`} alt="selected-top" fill/>
+              </div>
+            </div>
+            <div className="flex h-full justify-center">
+            <div className="relative aspect-square">
+              <Image className="" src={`/img/avatar/middle_${pngMiddle}.png`} alt="selected-top" fill/>
+              </div>
+            </div>
+            <div className="flex h-full justify-center">
+            <div className="relative aspect-square">
+              <Image className="" src={`/img/avatar/bottom_${pngBottom}.png`} alt="selected-top" fill/>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col h-full justify-around">
+            <div className="flex gap-8 justify-center">
+              <button 
+                className={`relative h-20 w-20 rounded-full shadow-xl ring-4 bg-white overflow-hidden ${pngTop==1?'ring-amber-300':'ring-white'}`}
+                onClick={() => setPngTop(1)} 
+              >
+                <Image className="p-2" src="/img/avatar/top_1.png" alt="top1" fill/>
+              </button>
+              <button 
+                className={`relative h-20 w-20 rounded-full shadow-xl ring-4 bg-white overflow-hidden ${pngTop==2?'ring-amber-300':'ring-white'}`}
+                onClick={() => setPngTop(2)} 
+                >
+                <Image className="p-2" src="/img/avatar/top_2.png" alt="top2" fill/>
+              </button>
+              <button
+                className={`relative h-20 w-20 rounded-full shadow-xl ring-4 bg-white overflow-hidden ${pngTop==3?'ring-amber-300':'ring-white'}`}
+                onClick={() => setPngTop(3)}
+                >
+                <Image className="p-2" src="/img/avatar/top_3.png" alt="top3" fill/>
+              </button>
+            </div>
+            <div className="flex gap-8 justify-center">
+              <button 
+                className={`relative h-20 w-20 rounded-full shadow-xl ring-4 bg-white overflow-hidden ${pngMiddle==1?'ring-amber-300':'ring-white'}`}
+                onClick={() => setPngMiddle(1)} 
+              >
+                <Image className="p-2" src="/img/avatar/middle_1.png" alt="mid1" fill/>
+              </button>
+              <button 
+                className={`relative h-20 w-20 rounded-full shadow-xl ring-4 bg-white overflow-hidden ${pngMiddle==2?'ring-amber-300':'ring-white'}`}
+                onClick={() => setPngMiddle(2)} 
+              >
+                <Image className="p-2" src="/img/avatar/middle_2.png" alt="mid2" fill/>
+              </button>
+              <button 
+                className={`relative h-20 w-20 rounded-full shadow-xl ring-4 bg-white overflow-hidden ${pngMiddle==3?'ring-amber-300':'ring-white'}`}
+                onClick={() => setPngMiddle(3)} 
+              >
+                <Image className="p-2" src="/img/avatar/middle_3.png" alt="mid3" fill/>
+              </button>
+            </div>
+            <div className="flex gap-8 justify-center">
+              <button 
+                className={`relative h-20 w-20 rounded-full shadow-xl ring-4 bg-white overflow-hidden ${pngBottom==1?'ring-amber-300':'ring-white'}`}
+                onClick={() => setPngBottom(1)}
+                >
+                <Image className="p-2" src="/img/avatar/bottom_1.png" alt="bot1" fill/>
+              </button>
+              <button 
+                className={`relative h-20 w-20 rounded-full shadow-xl ring-4 bg-white overflow-hidden ${pngBottom==2?'ring-amber-300':'ring-white'}`}
+                onClick={() => setPngBottom(2)} 
+                >
+                <Image className="p-2" src="/img/avatar/bottom_2.png" alt="bot2" fill/>
+              </button>
+              <button 
+                className={`relative h-20 w-20 rounded-full shadow-xl ring-4 bg-white overflow-hidden ${pngBottom==3?'ring-amber-300':'ring-white'}`}
+                onClick={() => setPngBottom(3)} 
+                >
+                <Image className="p-2" src="/img/avatar/bottom_3.png" alt="bot3" fill/>
+              </button>
+            </div>
+          </div>
+        </div>
+        <button
+        type="button"
+        className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        onClick={() => print()}
+        >
+          Imprimer
+        </button>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="hidden print:flex flex-col w-full justify-center items-center gap-8">
+        <p className="text-xl font-bold">Ta carte à découper</p>
+        <div
+          className="flex w-[85.6mm] h-[53.98mm] border-2 border-dotted border-black bg-white px-4"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div className="flex flex-col text-black aspect-[3/4] p-2">
+            <div className="flex h-full justify-center">
+              <div className="relative aspect-square">
+                <Image className="" src={`/img/avatar/top_${pngTop}.png`} alt="selected-top" fill/>
+              </div>
+            </div>
+            <div className="flex h-full justify-center">
+            <div className="relative aspect-square">
+              <Image className="" src={`/img/avatar/middle_${pngMiddle}.png`} alt="selected-top" fill/>
+              </div>
+            </div>
+            <div className="flex h-full justify-center">
+            <div className="relative aspect-square">
+              <Image className="" src={`/img/avatar/bottom_${pngBottom}.png`} alt="selected-top" fill/>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col text-black h-full w-full items-start justify-center gap-2">
+            <p className="border border-black font-semibold tracking-wide mb-4 p-4 w-full text-center">Logo</p>
+            <p className="font-semibold tracking-wide">{name1}</p>
+            <p className="text-xs">Membre N°{num}</p>
+          </div>
+        </div>
+        
       </div>
     </main>
   );
